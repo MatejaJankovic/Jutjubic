e40342a5ec7a36421e67488d3ae1c5cf2c0542f0
damjanxp <damjanpetrov53@gmail.com>
2025-12-28 09:21:56 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+\# Jutjubic (ISA)
+
+
+
+\## Run backend (Spring Boot)
+
+cd backend
+
+mvnw.cmd spring-boot:run
+
+
+
+Backend: http://localhost:8080
+
+
+
+\## Run frontend (Angular)
+
+cd frontend
+
+npm install
+
+npm start
+
+
+
+Frontend: http://localhost:4200
+
+
+
---
+++ jutjubic-backend/src/main/java/rs/ftn/isa/jutjubicbackend/JutjubicBackendApplication.java
@@ -0,0 +1,13 @@
+package rs.ftn.isa.jutjubicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JutjubicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JutjubicBackendApplication.class, args);
+    }
+
+}
